53372f84e165580cc4e323a83232174b020fc13b
fwijctfs <203466369+fwijctfs@users.noreply.github.com>
2025-04-13 17:27:49 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+// JavaScript (script.js)
+
+document.addEventListener('mousemove', function(e) {
+    const mouseX = e.pageX;
+    const mouseY = e.pageY;
+
+    const cursor = document.querySelector('.cursor-effect');
+
+    cursor.style.left = `${mouseX}px`;
+    cursor.style.top = `${mouseY}px`;
+
+    cursor.classList.add('active');
+
+    setTimeout(function() {
+        cursor.classList.remove('active');
+    }, 400);
+});
